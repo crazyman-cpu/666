23618ca4b9bd9fadaaddfdbe9da3e8d336d6f11c
crazyman-cpu <56492903+crazyman-cpu@users.noreply.github.com>
2019-10-21 09:41:22 +0800

    Update 4.php

--- 4.php
+++ 4.php
@@ -20,3 +20,15 @@ hkcms使用手册复制：
     </ul>
     分页：{$pages}
 {/hkcms:get}
+
+参数	说明
+@sql	SQL语句，强烈建议只用于select类型语句，其他SQL有严重安全威胁，同时不建议直接在SQL语句中使用外部变量，如:$_GET,$_POST等。出现“>”，“，{lt}代替，{elt}代替=
+@page	当前分页号，默认$page，当传入该参数表示启用分页，一个页面只允许有一个page，多个标签使用多个page会造成不可预知的问题
+@num	每次返回数据量
+@cache	数据缓存时间，单位秒
+@return	返回值变量名称，默认data
+@pagefun	分页函数，默认page()
+@pagetp	分页模板
+@table	表名，不用填写表前缀，当存在@table参数时，忽略@sql参数
+@order	排序参数，只有配合@table参数有效
+@where	SQL的where部分条件，只有配合@table参数有效
