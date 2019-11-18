56709ee2c66d02aa5c4cc84fadd43ea7957c92d4
crazyman-cpu <56492903+crazyman-cpu@users.noreply.github.com>
2019-11-18 11:34:18 +0800

    Create 1118.php

---
+++ 1118.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<style type="text/css">
+		body{
+			background: url(images/bd-bg.png) repeat-x;
+		}
+		.header{
+			width: 940px;
+			margin: 0 auto;
+			padding-top: 37px;
+			padding-bottom: 46px;
+		}
+		.nav{
+			float: right;
+			margin-top: 15px; 
+		}
+		.nav li{
+			float: left;
+			padding: 0 10px;
+			border-left: 1px dotted #d6d6d6
+		}
+		.nav a{
+			font: bold 14px Georgia;
+			color: #666666;
+			text-decoration: none;
+		}
+		.nav a:hover{
+			color: #ce7e2b;
+		}
+		.nav p{
+			color: #b7b7b7;
+			font: 11px Tahoma;
+		}
+		.logo{
+			margin-left: 15px;
+		}
+	</style>
+</head>
+<body>
+	<div class="header">
+		<ul class="nav">
+			<li>
+				<a href="#">HOME</a>
+				<p>back to home</p>
+			</li>
+			<li>
+				<a href="#">HOME</a>
+				<p>back to home</p>
+			</li>
+
+			<li>
+				<a href="#">HOME</a>
+				<p>back to home</p>
+			</li>
+
+			<li>
+				<a href="#">HOME</a>
+				<p>back to home</p>
+			</li>
+
+			<li>
+				<a href="#">HOME</a>
+				<p>back to home</p>
+			</li>
+		</ul>
+		<div class="logo">
+			<a href="#" title="logo">
+				<img src="images/logo.png" alt="logo">
+			</a>
+		</div>
+	</div> 
+</body>
+</html>
